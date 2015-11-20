b516189084356ea2ed287f0aa0c90f935a60b31c
梁继明 <ljm5000@163.com>
2015-11-20 16:15:24 +0800

    no message

---
+++ JMImagePicker/JMImagePicker/JMPickerViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMPickerViewController.h
+//  JMImagePicker
+//
+//  Created by 梁继明 on 15/11/12.
+//  Copyright © 2015年 梁继明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMPickerViewController : UIViewController
+
+@end
